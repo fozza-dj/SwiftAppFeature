3fd663e66a5b7c11af6cd05a2e77f9277876c721
fozza <fozza0611@outlook.com>
2024-05-28 00:40:13 +0800

    Initial Commit

---
+++ SwiftAppFeature/SwiftAppFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppFeatureApp.swift
+//  SwiftAppFeature
+//
+//  Created by fozza on 2024/5/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
